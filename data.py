60f6efa7dfea7c4f2896d70c7f64de4026a3effa
Fadel <fadelnuariko@gmail.com>
2019-11-23 01:09:50 +0700

    init

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles =[
+        {
+            'id':1,
+            'title':'Article 1',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author' : 'Fadel',
+            'created_date' : '21-09-2019'
+        },
+        {
+            'id':2,
+            'title':'Article 2',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author' : 'Nuariko',
+            'created_date' :'19-10-2018'
+        },
+        {
+            'id':3,
+            'title':'Article 3',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author' : 'Joko',
+            'created_date' : '12-07-2017'
+        }
+    ]
+
+    return articles
